6e023330ac8ad11106e1f87b4920a47e99d8f584
Luis Gomez Sanchez <luisgs@microsoft.com>
2019-12-09 21:10:32 +0100

    zzz

--- IncidentProcessing/GetVSTSState.cs
+++ IncidentProcessing/GetVSTSState.cs
@@ -14,6 +14,7 @@ namespace IncidentProcessing
 {
     public static class GetVSTSState
     {
+
         [FunctionName("GetVSTSState")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
