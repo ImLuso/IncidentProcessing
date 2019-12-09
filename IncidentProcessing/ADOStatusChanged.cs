41945e4b8b92759b7b14ef62ec338fc0744ce3bf
Luis Gomez Sanchez <luisgs@microsoft.com>
2019-12-09 22:23:26 +0100

    test

--- IncidentProcessing/ADOStatusChanged.cs
+++ IncidentProcessing/ADOStatusChanged.cs
@@ -26,9 +26,7 @@ namespace IncidentProcessing
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return (ActionResult)new OkObjectResult($"Hello, {name}");
         }
     }
 }
